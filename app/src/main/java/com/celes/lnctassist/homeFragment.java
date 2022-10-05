13b436d58a79d85b0792ee86e06956b64ed85ea8
Ayush Tripathi <tripathi.at02@gmail.com>
2022-10-06 00:13:53 +0530

    LNCT Assist App

--- app/src/main/java/com/celes/lnctassist/homeFragment.java
+++ app/src/main/java/com/celes/lnctassist/homeFragment.java
@@ -7,13 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class homeFragment extends Fragment {
     View view;
+    TextView textView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);
+        textView=view.findViewById(R.id.exampleTV);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(textView.getContext(), "Working", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return view;
     }
 }
