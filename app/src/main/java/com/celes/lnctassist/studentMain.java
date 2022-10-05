42ba58c407bf888f795c7f725c019916e034b012
Ayush Tripathi <tripathi.at02@gmail.com>
2022-10-06 02:46:55 +0530

    LNCT Assist App

--- app/src/main/java/com/celes/lnctassist/studentMain.java
+++ app/src/main/java/com/celes/lnctassist/studentMain.java
@@ -2,12 +2,16 @@ package com.celes.lnctassist;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -15,11 +19,15 @@ import com.google.android.material.navigation.NavigationBarView;
 public class studentMain extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main);
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         replaceFragment(new homeFragment());
 
         bottomNavigationView=findViewById(R.id.bottomNavigationView);
@@ -46,5 +54,24 @@ public class studentMain extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frameLayout,fragment);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.action_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Toast.makeText(studentMain.this, "WorkingAbout", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        return super.onOptionsItemSelected(item);
     }
 }
