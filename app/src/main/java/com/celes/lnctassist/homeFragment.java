58e82a76077862b865dc2ccb1e8868409e0217e8
Ayush Tripathi <tripathi.at02@gmail.com>
2022-10-05 23:54:40 +0530

    LNCT Assist App

---
+++ app/src/main/java/com/celes/lnctassist/homeFragment.java
@@ -0,0 +1,19 @@
+package com.celes.lnctassist;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class homeFragment extends Fragment {
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/celes/lnctassist/studentMain.java
+++ app/src/main/java/com/celes/lnctassist/studentMain.java
@@ -1,14 +1,50 @@
 package com.celes.lnctassist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class studentMain extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main);
+        replaceFragment(new homeFragment());
+
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.home:
+                        replaceFragment(new homeFragment());
+                        break;
+                    case R.id.trackComp:
+                        replaceFragment(new trackCompFragment());
+                        break;
+                    case R.id.profile:
+                        replaceFragment(new profileFragment());
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
