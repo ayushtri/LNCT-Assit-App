46da88af7f73fef2817fc6090ba07fe5aeb9d55c
Ayush Tripathi <tripathi.at02@gmail.com>
2022-10-31 18:36:19 +0530

    LNCT Assist App

--- app/src/main/java/com/celes/lnctassist/User.java
+++ app/src/main/java/com/celes/lnctassist/User.java
@@ -10,4 +10,28 @@ public class User {
         this.enrollment = enrollment;
         this.email = email;
     }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(String enrollment) {
+        this.enrollment = enrollment;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- app/src/main/java/com/celes/lnctassist/signUp.java
+++ app/src/main/java/com/celes/lnctassist/signUp.java
@@ -19,8 +19,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 public class signUp extends AppCompatActivity {
     EditText fullname, enrollment, email, password, cpassword;
@@ -31,6 +36,7 @@ public class signUp extends AppCompatActivity {
     FirebaseAuth mAuth;
     //FirebaseUser mUser;
     DatabaseReference databaseReference;
+    boolean enrollExist = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +55,41 @@ public class signUp extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                PerformAuth();
+                    checkEnrollExist();
             }
         });
+    }
+
+    private void checkEnrollExist() {
+        enrollExist = false;
+        FirebaseDatabase.getInstance().getReference("User")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                            String enroll=enrollment.getText().toString().trim();
+                            User user = dataSnapshot.getValue(User.class);
+                            String chEnroll = user.getEnrollment();
+                            if(enroll.equals(chEnroll)){
+                                enrollExist = true;
+                                break;
+                            }
+                        }
+                        if(enrollExist){
+                            enrollment.setError("Enrollment Already Registered");
+                            enrollment.requestFocus();
+                        }
+                        else{
+                            PerformAuth();
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 
     private void PerformAuth() {
@@ -123,7 +161,7 @@ public class signUp extends AppCompatActivity {
                                 });
                     }
                     else{
-                        Toast.makeText(signUp.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(signUp.this, "Email Already Exists", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
